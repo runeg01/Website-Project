062db39b7e1d6100961212336aef920d704602a8
runeg01 <danny_g9@hotmail.co.uk>
2018-07-19 11:50:43 +0100

    updated modal answer

--- js/quiz.js
+++ js/quiz.js
@@ -1,30 +1,38 @@
 //only allows one checkbox option to be selected
-$('input[type="checkbox"]').on('change', function() {
-   $(this).siblings('input[type="checkbox"]').prop('checked', false);
+$('input[type="checkbox"]').on('change', function () {
+    $(this).siblings('input[type="checkbox"]').prop('checked', false);
 });
 
 //submit function to add all user's answers to array
-function submit(){
-  answerArr = []
-$("input:checked").each(function(){
-    answerArr.push($(this).val());
-});
-console.log(answerArr);  
+function submit() {
+    answerArr = []
+    $("input:checked").each(function () {
+        answerArr.push($(this).val());
+    });
+    console.log(answerArr);
 }
 $("#answers").hide();
-$("#submit").click(function(){
+$("#submit").click(function () {
     $("#answers").show();
 })
+correctArr = ["C", "C", "C", "C", "C"]
 
-function viewResults(){
-    correctArr = ["C","C","C","C","C"]
+function viewResults() {
+
     $("#answer1").append(answerArr[0])
     $("#answer2").append(answerArr[1])
     $("#answer3").append(answerArr[2])
     $("#answer4").append(answerArr[3])
     $("#answer5").append(answerArr[4])
-    //if(answerArr.sort().join(',') === correctArr.sort().join(',')){
-        
-    //}else
+
+    const result = answerArr.filter((ans) => {
+        return correctArr.includes(ans)
+    }).length
+    const resultperc = (result / correctArr.length * 100).toFixed(2) + '%'
+    $("#perc").append("<p>" + resultperc + "</p>")
+    console.log(resultperc);
+}
+for (var i = 0; i < correctArr.length; i++) {
+    $("#correct").append("<p>" + correctArr[i] + "</p>")
 }
 
