993073be260eb542c3c432424e4e355df3554026
runeg01 <danny_g9@hotmail.co.uk>
2018-07-19 12:38:34 +0100

    updated card with compliment text

--- js/quiz.js
+++ js/quiz.js
@@ -11,12 +11,14 @@ function submit() {
     });
     console.log(answerArr);
 }
+
 $("#answers").hide();
 $("#submit").click(function () {
     $("#answers").show();
 })
 correctArr = ["C", "C", "C", "C", "C"]
 $("#results").hide();
+
 function viewResults() {
     $("#results").show();
     $("#answer1").append(answerArr[0])
@@ -28,11 +30,21 @@ function viewResults() {
     const result = answerArr.filter((ans) => {
         return correctArr.includes(ans)
     }).length
+    const resultComp = (result /correctArr.length * 100)
+    console.log(resultComp);
     const resultperc = (result / correctArr.length * 100).toFixed(2) + '%'
     $("#perc").append("<p>" + resultperc + "</p>")
     console.log(resultperc);
+    
+    if (resultComp >= 80){
+        $("#perc").append("<p>Well done!</p>")
+    }else if (resultComp >=60 && resultComp < 80){
+        $("#perc").append("<p>Getting there!</p>")
+    }else{
+        $("#perc").append("<p>Need more practice!</p>")
+    }
 }
+
 for (var i = 0; i < correctArr.length; i++) {
     $("#correct").append("<p>" + correctArr[i] + "</p>")
 }
-
